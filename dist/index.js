6bbabd5f0bc61ed1d09a7352063c86fae1d24c99
samuelprasad-yubi <samuel.prasad@go-yubi.com>
2023-07-02 13:03:36 +0530

    c

--- dist/index.js
+++ dist/index.js
@@ -9,15 +9,21 @@ const app = (0, express_1.default)();
 app.use(cors());
 const port = process.env.PORT || 8080;
 app.use((_, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
     next();
 });
-app.get('/', (_req, res) => {
-    return res.send('Express Typescript on Vercel');
+app.get("/", (_req, res) => {
+    return res.send({
+        name: "samuel",
+        age: 27,
+    });
 });
-app.get('/ping', (_req, res) => {
-    return res.send('pong 🏓');
+app.get("/ping", (_req, res) => {
+    return res.send({
+        name: "john",
+        age: 23,
+    });
 });
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,32 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from "express";
 const cors = require("cors");
-const app = express()
+const app = express();
 app.use(cors());
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
 app.use((_, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  next();
 });
 
-app.get('/', (_req: Request, res: Response) => {
-	return res.send('Express Typescript on Vercel')
-})
+app.get("/", (_req: Request, res: Response) => {
+  return res.send({
+    name: "samuel",
+    age: 27,
+  });
+});
 
-app.get('/ping', (_req: Request, res: Response) => {
-	return res.send('pong 🏓')
-})
+app.get("/ping", (_req: Request, res: Response) => {
+  return res.send({
+    name: "john",
+    age: 23,
+  });
+});
 
 app.listen(port, () => {
-	return console.log(`Server is listening on ${port}`)
-})
+  return console.log(`Server is listening on ${port}`);
+});
